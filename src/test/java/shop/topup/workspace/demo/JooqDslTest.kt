bac472710e4d79fc9e52f0481f7c1f0ba498b64b
Libing Chen <libing.chen@gmail.com>
2024-11-13 17:09:54 +0800

    test: extends TestcontainersBaseTest

--- src/test/java/shop/topup/workspace/demo/JooqDslTest.kt
+++ src/test/java/shop/topup/workspace/demo/JooqDslTest.kt
@@ -4,7 +4,7 @@ import com.github.database.rider.core.api.dataset.DataSet
 import org.jooq.DSLContext
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import shop.topup.workspace.InfraBaseTest
+import shop.topup.workspace.TestcontainersBaseTest
 import shop.topup.workspace.domain.common.jooq.Tables
 import shop.topup.workspace.domain.common.jooq.tables.records.AccountRecord
 
@@ -14,7 +14,7 @@ import shop.topup.workspace.domain.common.jooq.tables.records.AccountRecord
  * @author linux_china
  */
 @DataSet("/db/dataset/accounts.xml")
-class JooqDslTest : InfraBaseTest() {
+class JooqDslTest : TestcontainersBaseTest() {
     @Autowired
     lateinit var jooq: DSLContext
 
