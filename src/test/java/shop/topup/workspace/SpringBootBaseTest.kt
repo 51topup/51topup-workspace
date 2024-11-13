d35a6f3ee85f1cc13ba66db0bceb4e9d39c88563
Libing Chen <libing.chen@gmail.com>
2024-11-13 17:10:05 +0800

    test: SpringBootBaseTest

---
+++ src/test/java/shop/topup/workspace/SpringBootBaseTest.kt
@@ -0,0 +1,9 @@
+package shop.topup.workspace
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+abstract class SpringBootBaseTest {
+}
