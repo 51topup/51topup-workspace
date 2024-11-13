96cefa204d363aa6501357960d07d79a59104e33
Libing Chen <libing.chen@gmail.com>
2024-11-13 18:09:49 +0800

    test: AccountDAOTest

---
+++ src/test/java/shop/topup/workspace/domain/common/jooq/dao/AccountDAOTest.kt
@@ -0,0 +1,18 @@
+package shop.topup.workspace.domain.common.jooq.dao
+
+import com.github.database.rider.core.api.dataset.DataSet
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import shop.topup.workspace.TestcontainersBaseTest
+
+@DataSet("/db/dataset/accounts.xml")
+class AccountDAOTest : TestcontainersBaseTest() {
+    @Autowired
+    lateinit var accountDAO: AccountDAO
+
+    @Test
+    fun testFindById() {
+        val account = accountDAO.findById(1L)
+        println(account.get().id)
+    }
+}
