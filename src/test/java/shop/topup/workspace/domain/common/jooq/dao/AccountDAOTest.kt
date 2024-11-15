7f3c3291df6456f277cab6ea5b6cb6645719e828
Libing Chen <libing.chen@gmail.com>
2024-11-15 20:38:32 +0800

    test: add account extension test

--- src/test/java/shop/topup/workspace/domain/common/jooq/dao/AccountDAOTest.kt
+++ src/test/java/shop/topup/workspace/domain/common/jooq/dao/AccountDAOTest.kt
@@ -4,6 +4,7 @@ import com.github.database.rider.core.api.dataset.DataSet
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import shop.topup.workspace.TestcontainersBaseTest
+import shop.topup.workspace.domain.common.account
 
 @DataSet("/db/dataset/accounts.xml")
 class AccountDAOTest : TestcontainersBaseTest() {
@@ -14,5 +15,7 @@ class AccountDAOTest : TestcontainersBaseTest() {
     fun testFindById() {
         val account = accountDAO.findById(1L)
         println(account.get().id)
+        println(1L.account?.id)
     }
+
 }
