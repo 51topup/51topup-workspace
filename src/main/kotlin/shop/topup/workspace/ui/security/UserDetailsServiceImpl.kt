de67a50101c15edfe8fd9815d1cf79901519c756
Libing Chen <libing.chen@gmail.com>
2024-11-16 21:48:39 +0800

    chore: add nick, phone and email support

--- src/main/kotlin/shop/topup/workspace/ui/security/UserDetailsServiceImpl.kt
+++ src/main/kotlin/shop/topup/workspace/ui/security/UserDetailsServiceImpl.kt
@@ -1,27 +1,20 @@
 package shop.topup.workspace.ui.security
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import shop.topup.workspace.domain.common.jooq.dao.AccountDAO
+import shop.topup.workspace.domain.common.account
 import shop.topup.workspace.domain.common.jooq.tables.records.AccountRecord
 
 @Service
-class UserDetailsServiceImpl(val accountDAO: AccountDAO) : UserDetailsService {
+class UserDetailsServiceImpl : UserDetailsService {
 
     @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(username: String): UserDetails {
-        val user = if (username.matches(Regex("^[0-9]{11}$"))) {
-            accountDAO.findByPhone(username)
-                ?: throw UsernameNotFoundException("No user present with username: $username")
-        } else {
-            accountDAO.findByNick(username)
-                ?: throw UsernameNotFoundException("No user present with username: $username")
-        }
+        val user = username.account ?: throw UsernameNotFoundException("No user present with username: $username")
         return User(user.mobilePhone, user.hashedPassword, getAuthorities(user))
     }
 
