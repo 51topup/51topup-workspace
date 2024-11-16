47c22ba53d76b79cc331b8a7e8d50bff406eba0d
Libing Chen <libing.chen@gmail.com>
2024-11-16 15:31:15 +0800

    app: DashboardView

---
+++ src/main/java/shop/topup/workspace/ui/views/DashboardView.kt
@@ -0,0 +1,27 @@
+package shop.topup.workspace.ui.views
+
+import com.github.mvysny.karibudsl.v10.KComposite
+import com.github.mvysny.karibudsl.v10.h2
+import com.github.mvysny.karibudsl.v10.verticalLayout
+import com.vaadin.flow.router.Route
+import com.vaadin.flow.router.RouteAlias
+import shop.topup.workspace.ui.layout.WorkspaceLayout
+
+
+@Route(value = "", layout = WorkspaceLayout::class)
+@RouteAlias(value = "/workspace", layout = WorkspaceLayout::class)
+class DashboardView : KComposite() {
+    // The main view UI definition
+    private val root = ui {
+        // Use custom CSS classes to apply styling. This is defined in styles.css.
+        verticalLayout(classNames = "centered-content") {
+            h2("Dashboard")
+        }
+    }
+
+    init {
+        // attach functionality to the UI components.
+        // It's a good practice to keep UI functionality separated from UI definition.
+
+    }
+}
