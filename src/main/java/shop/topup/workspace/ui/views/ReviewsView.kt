ba68a58db4a926a21e0a9a957c0ef1ff0b1093f4
Libing Chen <libing.chen@gmail.com>
2024-11-16 20:09:07 +0800

    app: reviews view

---
+++ src/main/java/shop/topup/workspace/ui/views/ReviewsView.kt
@@ -0,0 +1,35 @@
+package shop.topup.workspace.ui.views
+
+import com.github.mvysny.karibudsl.v10.KComposite
+import com.github.mvysny.karibudsl.v10.h2
+import com.github.mvysny.karibudsl.v10.verticalLayout
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.HasDynamicTitle
+import com.vaadin.flow.router.PageTitle
+import com.vaadin.flow.router.Route
+import jakarta.annotation.security.RolesAllowed
+import shop.topup.workspace.ui.layout.WorkspaceLayout
+import shop.topup.workspace.ui.security.Role
+
+@RolesAllowed(Role.SELLER)
+@Route(value = "/reviews", layout = WorkspaceLayout::class)
+@PageTitle("评价管理")
+class ReviewsView : KComposite() {
+    private lateinit var nameField: TextField
+    private lateinit var greetButton: Button
+
+    // The main view UI definition
+    private val root = ui {
+        // Use custom CSS classes to apply styling. This is defined in styles.css.
+        verticalLayout(classNames = "centered-content") {
+            h2("评价管理")
+        }
+    }
+
+    init {
+        // attach functionality to the UI components.
+        // It's a good practice to keep UI functionality separated from UI definition.
+    }
+
+}
