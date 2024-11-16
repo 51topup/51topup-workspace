25692275b587c2ee6fcf772efdd8a0867d50fd8c
Libing Chen <libing.chen@gmail.com>
2024-11-16 16:30:32 +0800

    chore: add columns

--- src/test/resources/db/migration/V001__create_member.sql
+++ src/test/resources/db/migration/V001__create_member.sql
@@ -1,11 +1,13 @@
 create table account
 (
     id              bigserial primary key,
+    nick            varchar(100) not null,
     mobile_phone    varchar(32)  not null,
+    email           varchar(128),
     hashed_password varchar(100) not null,
     first_name      varchar(100) not null,
     last_name       varchar(100) not null,
-    role_names      varchar(100) not null,
+    roles           varchar(100) not null,
     type            int          not null, -- 0: seller, 1: admin, 2: service provider
     status          int          not null,
     flag            bigint,
