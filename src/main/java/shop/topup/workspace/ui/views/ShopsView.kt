513288e231e34b200e289e96be29c14597d9dbd6
Libing Chen <libing.chen@gmail.com>
2024-11-16 15:32:18 +0800

    app(ui): ShopsView

---
+++ src/main/java/shop/topup/workspace/ui/views/ShopsView.kt
@@ -0,0 +1,50 @@
+package shop.topup.workspace.ui.views
+
+import com.github.mvysny.karibudsl.v10.*
+import com.github.mvysny.kaributools.setPrimary
+import com.vaadin.flow.component.Key
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.HasDynamicTitle
+import com.vaadin.flow.router.Route
+import shop.topup.workspace.ui.layout.WorkspaceLayout
+
+@Route(value = "/shops", layout = WorkspaceLayout::class)
+class ShopsView : KComposite(), HasDynamicTitle {
+    private lateinit var nameField: TextField
+    private lateinit var greetButton: Button
+
+    // The main view UI definition
+    private val root = ui {
+        // Use custom CSS classes to apply styling. This is defined in styles.css.
+        verticalLayout(classNames = "centered-content") {
+
+            // Use TextField for standard text input
+            nameField = textField("Your name:") {
+                addClassName("bordered")
+            }
+
+            // Use Button for a clickable button
+            greetButton = button("Say main") {
+                setPrimary()
+                addClickShortcut(Key.ENTER)
+            }
+        }
+    }
+
+    init {
+        // attach functionality to the UI components.
+        // It's a good practice to keep UI functionality separated from UI definition.
+
+        // Button click listeners can be defined as lambda expressions
+        greetButton.onClick {
+            Notification.show("Hello, ${nameField.value}")
+        }
+    }
+
+    override fun getPageTitle(): String {
+        return "店铺管理"
+    }
+
+}
