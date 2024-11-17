25c603b1eaea8488815fa10c7b35ba086b573188
Libing Chen <libing.chen@gmail.com>
2024-11-17 21:34:05 +0800

    chore: change role to upper case

--- src/main/kotlin/shop/topup/workspace/ui/security/Role.kt
+++ src/main/kotlin/shop/topup/workspace/ui/security/Role.kt
@@ -1,6 +1,6 @@
 package shop.topup.workspace.ui.security
 
 object Role {
-    const val SELLER: String = "seller"
-    const val ADMIN: String = "admin"
+    const val SELLER: String = "SELLER"
+    const val ADMIN: String = "ADMIN"
 }
