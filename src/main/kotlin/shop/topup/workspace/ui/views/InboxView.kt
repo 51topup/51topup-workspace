496cdf603758cc0ec88e4d0f13664df30b416318
Libing Chen <libing.chen@gmail.com>
2024-11-19 13:19:52 +0800

    app: inbox view

---
+++ src/main/kotlin/shop/topup/workspace/ui/views/InboxView.kt
@@ -0,0 +1,38 @@
+package shop.topup.workspace.ui.views
+
+import com.github.mvysny.karibudsl.v10.KComposite
+import com.github.mvysny.karibudsl.v10.h2
+import com.github.mvysny.karibudsl.v10.onClick
+import com.github.mvysny.karibudsl.v10.verticalLayout
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.PageTitle
+import com.vaadin.flow.router.Route
+import jakarta.annotation.security.RolesAllowed
+import shop.topup.workspace.ui.layout.WorkspaceLayout
+import shop.topup.workspace.ui.security.Role
+
+@RolesAllowed(Role.SELLER)
+@Route(value = "/inbox", layout = WorkspaceLayout::class)
+@PageTitle("站内信")
+class InboxView : KComposite() {
+    private lateinit var nameField: TextField
+    private lateinit var greetButton: Button
+
+    // The main view UI definition
+    private val root = ui {
+        // Use custom CSS classes to apply styling. This is defined in styles.css.
+        verticalLayout(classNames = "centered-content") {
+
+            h2("站内信")
+        }
+    }
+
+    init {
+        // attach functionality to the UI components.
+        // It's a good practice to keep UI functionality separated from UI definition.
+        
+    }
+
+}
