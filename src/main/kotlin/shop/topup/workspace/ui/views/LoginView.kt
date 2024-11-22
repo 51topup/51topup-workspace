75be0b42d290cbcd657d2f5d2f63b67fb5c30953
Libing Chen <libing.chen@gmail.com>
2024-11-22 16:40:18 +0800

    chore: add goofish login url

--- src/main/kotlin/shop/topup/workspace/ui/views/LoginView.kt
+++ src/main/kotlin/shop/topup/workspace/ui/views/LoginView.kt
@@ -1,5 +1,7 @@
 package shop.topup.workspace.ui.views
 
+import com.vaadin.flow.component.html.Anchor
+import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.login.LoginI18n
 import com.vaadin.flow.component.login.LoginOverlay
 import com.vaadin.flow.router.BeforeEnterEvent
@@ -9,8 +11,10 @@ import com.vaadin.flow.router.Route
 import com.vaadin.flow.router.internal.RouteUtil
 import com.vaadin.flow.server.VaadinService
 import com.vaadin.flow.server.auth.AnonymousAllowed
+import com.vaadin.flow.theme.lumo.LumoUtility
 import org.springframework.beans.factory.annotation.Autowired
 import shop.topup.workspace.ui.security.AuthenticatedUser
+import java.net.URLEncoder
 
 
 @Route(value = "/login")
@@ -26,6 +30,13 @@ class LoginView(loginI18n: LoginI18n) : LoginOverlay(loginI18n), BeforeEnterObse
         description = "一站式管理虚拟充值业务"
         action = RouteUtil.getRoutePath(VaadinService.getCurrent().context, javaClass)
         isForgotPasswordButtonVisible = false
+        val div = Div()
+        val link = Anchor(gooFishLoginUrl(), "闲鱼登录").apply {
+            element.setAttribute("style", "font-size:18px")
+        }
+        div.add(link)
+        div.addClassName(LumoUtility.TextAlignment.CENTER)
+        footer.add(div)
         isOpened = true
     }
 
@@ -37,5 +48,12 @@ class LoginView(loginI18n: LoginI18n) : LoginOverlay(loginI18n), BeforeEnterObse
         }
 
         isError = beforeEnterEvent.location.queryParameters.parameters.containsKey("error")
+    }
+
+    fun gooFishLoginUrl(): String {
+        val redirectUrl = URLEncoder.encode("https://xianyu.51topup.com/callback", "UTF-8")
+        val state = URLEncoder.encode("{\"origin\": \"main\"}", "UTF-8")
+        val clientId = "34912212"
+        return "https://oauth.taobao.com/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}&state=${state}&view=web"
     }
 }
