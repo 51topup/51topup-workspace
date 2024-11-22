a1b316ce43fb5311d88be135576300af1c951f08
Libing Chen <libing.chen@gmail.com>
2024-11-22 16:20:57 +0800

    chore: add goofish_profile table

--- src/test/resources/db/migration/V001__create_member.sql
+++ src/test/resources/db/migration/V001__create_member.sql
@@ -14,3 +14,18 @@ create table account
     created_at      timestamp    not null,
     updated_at      timestamp    not null
 );
+
+create table goofish_profile
+(
+    id                       bigserial primary key,
+    account_id               bigint       not null,
+    user_id                  varchar(100) not null,
+    user_nick                varchar(100) not null,
+    expire_time              timestamp    not null,
+    access_token             varchar(100) not null,
+    refresh_token            varchar(100) not null,
+    refresh_token_valid_time timestamp    not null,
+    status                   int          not null, -- 0: normal
+    created_at               timestamp    not null,
+    updated_at               timestamp    not null
+);
