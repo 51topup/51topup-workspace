134ec15d09e590d017510fbe137e9b5940b95edc
Libing Chen <libing.chen@gmail.com>
2024-11-24 15:00:56 +0800

    chore: polishing

--- src/main/kotlin/shop/topup/workspace/domain/infra/GooFishService.kt
+++ src/main/kotlin/shop/topup/workspace/domain/infra/GooFishService.kt
@@ -5,6 +5,7 @@ import com.taobao.api.TaobaoClient
 import com.taobao.api.request.AlibabaIdleIsvUserQueryRequest
 import com.taobao.api.request.TopAuthTokenCreateRequest
 import com.taobao.api.response.AlibabaIdleIsvUserQueryResponse
+import com.taobao.api.response.TopAuthTokenCreateResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
@@ -20,17 +21,15 @@ class GooFishService(
     @Suppress("LeakingThis")
     var topClient: TaobaoClient = DefaultTaobaoClient(topApiUrl, topAppKey, topAppSecret)
 
-    fun getAccessTokenResult(code: String): String {
+    fun getAccessTokenResult(code: String): TopAuthTokenCreateResponse {
         val req = TopAuthTokenCreateRequest()
         req.code = code
-        val rsp = topClient.execute(req)
-        return rsp.tokenResult
+        return topClient.execute(req)
     }
 
-    fun fetchSellerInfo(sessionKey: String): AlibabaIdleIsvUserQueryResponse.IdleUserApiDo {
+    fun fetchSellerInfo(sessionKey: String): AlibabaIdleIsvUserQueryResponse {
         val req = AlibabaIdleIsvUserQueryRequest()
-        val rsp = topClient.execute(req, sessionKey)
-        return rsp.result.data
+        return topClient.execute(req, sessionKey)
     }
 
 }
