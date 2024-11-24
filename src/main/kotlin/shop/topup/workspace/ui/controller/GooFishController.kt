50131d21c9ab2ba31b5caee3e467a2ed7980d87e
Libing Chen <libing.chen@gmail.com>
2024-11-24 14:07:16 +0800

    feat: implement login from goofish callback

--- src/main/kotlin/shop/topup/workspace/ui/controller/GooFishController.kt
+++ src/main/kotlin/shop/topup/workspace/ui/controller/GooFishController.kt
@@ -1,21 +1,45 @@
 package shop.topup.workspace.ui.controller
 
+import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import shop.topup.workspace.ui.security.JwtService
 
 @RestController
 @RequestMapping("/goofish")
-class GooFishController {
+class GooFishController(
+    val jwtService: JwtService,
+    @Value("\${jwt.expiration}") val expirationSeconds: Int
+) {
 
     @GetMapping("/callback")
-    fun hello(request: HttpServletRequest, response: HttpServletResponse): String {
+    fun hello(request: HttpServletRequest, response: HttpServletResponse) {
         request.getParameter("code")?.let {
-            //response.sendRedirect("/workspace")
+            //todo validate code by TOP platform
+            val jwt = jwtService.generate("seller1", "SELLER")
+            val headerAndPayload = jwt.substring(0, jwt.lastIndexOf("."))
+            val signature = jwt.substring(jwt.lastIndexOf(".") + 1)
+            response.addCookie(
+                Cookie(
+                    "jwt.headerAndPayload",
+                    headerAndPayload
+                ).apply {
+                    maxAge = expirationSeconds
+                    path = "/"
+                })
+            response.addCookie(Cookie("jwt.signature", signature).apply {
+                isHttpOnly = true
+                maxAge = expirationSeconds
+                path = "/"
+            })
+            response.sendRedirect("/workspace")
+            return
         }
-        //response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid code")
-        return "callback"
+        response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid code")
     }
 }
