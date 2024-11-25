6aab8153ac83cac2338958c08991e7c0abbc6fe5
Libing Chen <libing.chen@gmail.com>
2024-11-25 21:40:22 +0800

    chore: add reviewer info

--- src/test/resources/db/migration/V002__create_order.sql
+++ src/test/resources/db/migration/V002__create_order.sql
@@ -24,12 +24,14 @@ create table goo_fish_order
 -- 闲鱼订单评价
 create table goo_fish_order_rate
 (
-    id           bigserial primary key,
-    account_id   bigint    not null,
-    order_id     bigint    not null,
-    rate         int, -- 评价结果 1 好评 0 默认
-    feedback     varchar(256),
-    rate_type    int, -- 评价类型 1 好评 0 中评
-    gmt_create   timestamp not null,
-    gmt_modified timestamp not null
+    id            bigserial primary key,
+    account_id    bigint    not null,
+    order_id      bigint    not null,
+    rate          int,          -- 评价结果 1 好评 0 默认
+    feedback      varchar(256),
+    rate_type     int,          -- 评价类型 1 好评 0 中评
+    reviewer_nick varchar(100), -- 评价人昵称
+    reviewer_type int,          -- 评价人类型 1 买家 0 卖家
+    gmt_create    timestamp not null,
+    gmt_modified  timestamp not null
 )
